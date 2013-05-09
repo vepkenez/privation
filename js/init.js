63ced4a9f1f39d50daf58e557b240edf6bd78406
damon <gdamon@gmail.com>
2013-05-08 20:19:17 -0400

    added more complex queries

--- js/init.js
+++ js/init.js
@@ -7,7 +7,13 @@ var q_funcs = {
 
 	'diff': function(param,obj1,obj2){
 		return (obj1[param] != obj2[param])
-	}
+	},
+	'is': function(param,obj1,obj2,val){
+		return (obj2[param] == val)
+	},
+	'is_not': function(param,obj1,obj2,val){
+		return (obj2[param] != val)
+	},
 
 }
 
@@ -28,9 +34,17 @@ function get_pams(pam,query_object){
 		var o = query_object[q],
 			k = Object.keys(o)[0],
 			v = o[k],
+			p = null;
+
+		if (typeof(v)== 'object'){
+			var l = Object.keys(v)[0];
+			p = v[l];
+			v = l;
+		}
+
 
 		PL = _(PL).filter(function(thispam){
-			return q_funcs[v](k,pam,thispam);
+			return q_funcs[v](k,pam,thispam,p);
 		})
 
 	}
@@ -64,11 +78,20 @@ $(window).load(function () {
   					]
  		},
  		{
- 			'name': 'same action, different person',
+ 			'name': 'same person, different action',
+ 			'query':[
+ 						{'user_id':'same'},
+  						{'chain_id':'diff'}
+  						
+  					]
+ 		},
+ 		{
+ 			'name': 'same action, different person, user_id is not "5"',
  			'query':[
  						
   						{'chain_id':'same'},
-  						{'user_id':'diff'}
+  						{'user_id':'diff'},
+  						{'user_id': {'is_not':5}}
   					]
  		},
  		{
